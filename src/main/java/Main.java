74d951e06ec70d2b3c0e2a0de37cc3e3e1d42db2
M10beretta <Mberetta.gm@gmail.com>
2022-04-03 02:40:53 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 3, 4, 5};
-        Cat[] cats = {new Cat("A"),new Cat("B"),new Cat("C"),new Cat("D")};
+        Cat[] cats = {new Cat("A"), new Cat("B"), new Cat("C"), new Cat("D")};
 
 
-        System.out.println(min(3,3,5,76,4,434,3,5,35,3,53));
+        System.out.println(min(3, 3, 5, 76, 4, 434, 3, 5, 35, 3, 53));
 
     }
-
 
     static int min(int firstArg, int... remainingArgs) {
         int min = firstArg;
